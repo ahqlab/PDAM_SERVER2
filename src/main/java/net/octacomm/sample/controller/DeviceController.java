c2ee362ca91d7e16901eb6aa39957db96e1c5e52
정지윤 <163826760+jiyun040@users.noreply.github.com>
2026-07-30 16:46:31 +0900

    들여쓰기 수정

--- src/main/java/net/octacomm/sample/controller/DeviceController.java
+++ src/main/java/net/octacomm/sample/controller/DeviceController.java
@@ -340,8 +340,7 @@ public class DeviceController extends AbstractDeviceCRUDController<DeviceMapper,
 		
 	@ResponseBody
 	@RequestMapping(value = "/doDelete", method = RequestMethod.POST)
-	public boolean doDelete(@RequestParam("id") int id, HttpSession session,
-			HttpServletResponse response) {
+	public boolean doDelete(@RequestParam("id") int id, HttpSession session, HttpServletResponse response) {
 		Object role = session.getAttribute("role");
 		if (!(role instanceof Integer) || ((Integer) role).intValue() != 0) {
 			response.setStatus(HttpServletResponse.SC_FORBIDDEN);
